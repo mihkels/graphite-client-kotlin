45f6e2578e260cf08aaed5ed42d65d5fccfc4e7b
Mihkel Selgal <mihkel.selgal@hot.ee>
2017-09-20 17:02:02 +0300

    Streamlined and more modular graphite client

--- graphite-client/src/main/kotlin/com/mihkels/graphite/client/BasicGraphiteClient.kt
+++ graphite-client/src/main/kotlin/com/mihkels/graphite/client/BasicGraphiteClient.kt
@@ -1,63 +1,85 @@
 package com.mihkels.graphite.client
 
-import mu.KLogging
+import mu.KotlinLogging
 import java.io.IOException
 import java.io.PrintWriter
 import java.net.Socket
 import java.net.UnknownHostException
+import java.time.Instant
 
-data class GraphiteMetric(val metricName: String, val metricValue: String, val timestamp: Long)
+private val logger = KotlinLogging.logger {  }
 
-data class GraphiteSettings(val hostUrl: String = "localhost", val hostPort: Int = 2003)
+data class GraphiteClientProperties(
+        var hostname: String = "localhost",
+        var port: Int = 2003,
+        var metricPrefix: String = ""
+)
+
+data class GraphiteMetric(
+        val metricName: String,
+        val metricValue: String,
+        val timestamp: Long = Instant.now().toEpochMilli()
+)
 
 interface GraphiteClient {
     fun send(graphiteMetric: GraphiteMetric)
     fun send(graphiteMetrics: Collection<GraphiteMetric>)
 }
 
-class BasicGraphiteClient(private val graphiteSettings: GraphiteSettings): GraphiteClient {
-    companion object: KLogging()
+interface DataSender {
+    fun sendData(host: String = "", port: Int = 0, callback: (PrintWriter) -> Unit = {})
+}
 
+class SocketSender : DataSender {
+    override fun sendData(host: String, port: Int, callback: (PrintWriter) -> Unit) {
+        Socket(host, port).getOutputStream().use {
+            PrintWriter(it, true).use { callback(it) }
+        }
+    }
+}
+
+class BasicGraphiteClient(
+        private val properties: GraphiteClientProperties,
+        private val dataSender: DataSender = SocketSender()
+): GraphiteClient {
     override fun send(graphiteMetric: GraphiteMetric) {
-        logger.info { "Start Sending out metric: $graphiteMetric" }
         sender { writer ->
             val message = convertToSting(graphiteMetric)
-            logger.info { "Output message: $message" }
+            logger.debug { "Sending out message: $message" }
             writer.print(message)
         }
-        logger.info { "Finished sending out metric" }
     }
 
     override fun send(graphiteMetrics: Collection<GraphiteMetric>) {
-        logger.info { "Multi metric send started" }
-        sender { writer ->
-            graphiteMetrics.forEach {
-                logger.info { "Sending out: $it" }
-                writer.print(convertToSting(it))
+        logger.debug { "Start sending to graphite host: ${properties.hostname}:${properties.port}" }
+        logger.info { "start sending out Graphtite metrics with count: ${graphiteMetrics.size}" }
+        if (graphiteMetrics.isNotEmpty()) {
+            sender { writer -> graphiteMetrics
+                .map {
+                    val modified = convertToSting(it)
+                    logger.debug { "Sending out metric: $modified" }
+                    modified
+                }
+                .forEach { writer.print(it) }
             }
         }
-        logger.info { "Finished sending out multi metric to graphite" }
     }
 
     private fun sender(callback: (PrintWriter) -> Unit) {
         try {
-            val socket = Socket(graphiteSettings.hostUrl, graphiteSettings.hostPort)
-            logger.info { "Connection status: ${socket.isConnected}" }
-            val outputStream = socket.getOutputStream()
-            val writer = PrintWriter(outputStream, true)
-            callback(writer)
-
-            writer.close()
-            socket.close()
+            dataSender.sendData(properties.hostname, properties.port, callback)
         } catch (e: UnknownHostException) {
-            throw GraphiteException("Unknown host: ${graphiteSettings.hostUrl}")
+            throw GraphiteException("Unknown host: ${properties.hostname}")
         } catch (e: IOException) {
             throw GraphiteException("Error while writing data to graphite: ${e.message}")
         }
     }
 
+    private fun addPrefix(metricName: String): String =
+           if (properties.metricPrefix.isNotEmpty()) "${properties.metricPrefix}.$metricName" else metricName
+
     private fun convertToSting(metric: GraphiteMetric) =
-            "${metric.metricName} ${metric.metricValue} ${metric.timestamp}\n"
+            "${addPrefix(metric.metricName)} ${metric.metricValue} ${metric.timestamp / 1000}\n"
 }
 
 class GraphiteException(message: String) : Exception(message)
